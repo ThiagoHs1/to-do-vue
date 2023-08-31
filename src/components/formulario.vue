315d2ff3354b0fa7d0a433240a1c83bc9d6bd5ad
Istan <thiago.sh26@gmail.com>
2023-08-30 21:29:00 -0300

    Finalizando o app

---
+++ src/components/formulario.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps(['cadastrarTarefa', 'TarefaTemp', 'trocarFiltro', 'editaTarefaTemp'])
+</script>
+
+<template>
+<form @submit.prevent="props.cadastrarTarefa" >
+    <div class="row">
+        <div class="col">
+            <input required :value="props.tarefaTemp" @change="props.editaTarefaTemp" type="text" placeholder="Digite sua tarefa" class="form-control" name="" id="">
+        </div>
+            <div class="col-md-2">
+                <button class="btn btn-primary" type="submit">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select   @change ="props.trocarFiltro" name="" class="form-control" >
+                    <option value="Todas">Todas tarefas</option>
+                    <option value="Pendentes">Tarefas pendentes</option>
+                    <option value="Finalizadas">Tarefas finalizadas</option>
+                </select>
+            </div>
+        </div>
+</form>
+</template>
